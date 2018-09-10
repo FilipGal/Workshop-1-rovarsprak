d84d859af6c5a8c74be14bcba7d3f5428cd01a70
fg222ak <fg222ak@student.lnu.se>
2018-09-10 13:56:30 +0200

    Pushing stuff

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -1,19 +1,29 @@
 <?php
-class LayoutView {
-    public function render(InputView $iv, OutputView $ov) : string {
+class LayoutView
+{
+    /**
+     * Render page view
+     *
+     * @param InputView $iv
+     * @param OutputView $ov
+     * @return void
+     */
+    public function render(InputView $iv, OutputView $ov)
+    {
         echo '<!DOCTYPE html>
         <html>
             <head>
                 <meta charset="utf-8">
                 <title>Rovarsprak</title>
                 <link rel="stylesheet" href="style.css">
-                <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+                <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
             </head>
             <body>
-                '.$iv->render().'
-                '.$ov->render().'
+                ' . $iv->renderLanguageOption() . '
+                ' . $iv->renderInput() . '
+                ' . $ov->renderOutput() . '
             </body>
         </html>
-        '; 
+        ';
     }
 }
--- view/OutputView.php
+++ view/OutputView.php
@@ -1,13 +1,22 @@
 <?php
-require_once 'controller/RovarSprak.php';
+require_once 'controller/TranslateController.php';
 
-class OutputView {
-    private $rovarsprak;
+class OutputView
+{
+    private $translator;
 
-    function __construct() {
-        $this->rovarsprak = new RovarSprak();
+    public function __construct()
+    {
+        $this->translator = new TranslateController();
     }
-    public function render() : string {
-        return '<div class="output">'.$this->rovarsprak->render().'</div>';
+
+    /**
+     * Return translated text
+     *
+     * @return string
+     */
+    public function renderOutput(): string
+    {
+        return '<div class="output">' . $this->translator->displayTranslatedText() . '</div>';
     }
 }
